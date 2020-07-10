2db6ba493f8f3bbb685a05840199667bebae8f43
Hodosov <Hodosov.y@mail.ru>
2020-07-10 15:21:23 +0300

    add form

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar';
+import { AddTodo } from './src/AddTodo';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-                    <Navbar title='Todo App' />
-    </View>
-  );
+
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (title) => {
+
+        setTodos(prev => [...prev, {
+            id: Date.now().toString(),
+            title
+        }])
+    }
+
+    return (
+        <View>
+            <Navbar title='Todo App' />
+            <View style={styles.container}>
+                <AddTodo onSubmit={addTodo} />
+                <View>
+                    {todos.map(todo => <Text key={todo.id}>{todo.title}</Text>)}
+                </View>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  }
+    container: {
+        paddingHorizontal: 20
+    }
 });
