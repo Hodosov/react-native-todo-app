4ce0b4c40a7d0bf0be21a1b0e46ffc279e42130b
Hodosov <Hodosov.y@mail.ru>
2020-07-25 09:37:08 +0300

    fix alert

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,40 +1,14 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { View } from 'react-native'
 import { Navbar } from '../src/components/Navbar'
 import { MainScreen } from '../src/screens/MainScreen'
 import { TodoScreen } from '../src/screens/TodoScreen'
-import { TodoContext } from './context/todo/todoContext'
 import { ScreenContext } from './context/screen/screenContext'
 
 
 export const MainLayout = () => {
 
     const { todoId } = useContext(ScreenContext)
-
-    // const removeTodo = (id) => {
-
-    //     const todo = todos.find(el => el.id === id)
-
-    //     Alert.alert(
-    //         'Удаление элемента',
-    //         `Вы уверены что хотите удалить ${todo.title}?`,
-    //         [
-    //             {
-    //                 text: 'Отмена',
-    //                 style: 'cancel'
-    //             },
-    //             {
-    //                 text: 'Удалить',
-    //                 style: 'destructive',
-    //                 onPress: () => {
-    //                     setTodoId(null)
-    //                     setTodos(prev => prev.filter((todo) => todo.id !== id))
-    //                 }
-    //             }
-    //         ],
-    //         { cancelable: true }
-    //     );
-    // }
 
     return (
         <View>
--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,4 +1,5 @@
 import React, {useReducer, useContext} from 'react'
+import { Alert } from 'react-native'
 import {TodoContext} from './todoContext'
 import {todoReducer} from './todoReducer'
 import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
@@ -18,8 +19,31 @@ export const TodoState = ({children}) => {
 
     const addTodo = (title) => dispatch({type: ADD_TODO, title})
     const removeTodo = (id) => {
-        changeScreen(null)
-        dispatch({type: REMOVE_TODO, id })
+
+        const todo = state.todos.find(el => el.id === id)
+
+        Alert.alert(
+                    'Удаление элемента',
+                    `Вы уверены что хотите удалить ${todo.title}?`,
+                    [
+                        {
+                            text: 'Отмена',
+                            style: 'cancel'
+                        },
+                        {
+                            text: 'Удалить',
+                            style: 'destructive',
+                            onPress: () => {
+                                changeScreen(null)
+                                dispatch({type: REMOVE_TODO, id })
+                            }
+                        }
+                    ],
+                    { cancelable: true }
+                );
+
+
+       
     } 
     
     const updateTodo = (id, title) => dispatch({type: UPDATE_TODO, id, title})
